7bf68675aa6bf52028a10c80092b5b2726e297b2
xukang <lichterxk@gmail.com>
2021-11-07 23:32:27 +0800

    add all annotation

--- src/main.py
+++ src/main.py
@@ -7,48 +7,58 @@ from multiprocessing import Process
 
 
 def train(config: Dict):
-    svm = SVM(config)
-    loader = DataLoader(config)
+    """
+    Train function which open one experiment, witch will be called by Process to implement the parallel experiments
+
+    Args:
+        config (Dict): [the congit dict]
+    """
+    svm = SVM(config)                                               # Instantiate the SVM wrap 
+    loader = DataLoader(config)                                     # Instantiate the data loader
 
     loader.preprocess(
         inverse_color=config['using_inverse_color'], 
-        normalize=config['using_normalize']
-    )
+        normalize=config['using_normalize'] 
+    )                                                               # Preprocess the train_set and test_set images
 
     svm.train(
         data= loader.train_img[:config['train_sample_num']], 
         label= loader.train_label[:config['train_sample_num']]
-    )
+    )                                                               # Train the model
+
     svm.evaluate_and_save(
         data= loader.test_img[:5000],
         label= loader.test_label[:5000]
-    )
+    )                                                               # Evaluate the model, save model and report
 
 
 def run() -> None:
-    origin_config = load_config()
+    """
+    Main function to run the parallel experiments based on the congfig.json setting
+    """
+    origin_config = load_config()                                   # Read the config.json file
     
-    sample_num = origin_config['train_sample_num']
-    kernel = origin_config['kernel']
-    inverse_color = origin_config['using_inverse_color']
-    normalize = origin_config['using_normalize']
+    sample_num = origin_config['train_sample_num']                  # Get all sample_num parameters
+    kernel = origin_config['kernel']                                # Get all kernel parameters
+    inverse_color = origin_config['using_inverse_color']            # Get all inverse_color parameters 
+    normalize = origin_config['using_normalize']                    # Get all normalization parameters
 
-    for sample_n in sample_num:
+    for sample_n in sample_num:                                     # Do the grid search to get each combination of the parameters above
         for k in kernel:
             for inverse in inverse_color:
                 for normal in normalize:
-                    exp_name = f"sample-{sample_n}_kernel-{k}_inverse-{inverse}_normal-{normal}"
-                    config = copy(origin_config)
+                    exp_name = f"sample-{sample_n}_kernel-{k}_inverse-{inverse}_normal-{normal}"        # Using the parameter combination as the experiment name and direction
+                    config = copy(origin_config)                                                        # Copy the origin config dict
                     config.update({
                         'train_sample_num': sample_n,
                         'kernel': k,
                         'using_inverse_color': inverse,
                         'using_normalize': normal,
                         'save_path': origin_config['save_path'] + f'{exp_name}/'
-                    })
-                    p = Process(target= train, args=(config, ))
-                    p.start()
+                    })                                                                                  # Update the each parameter of the current experiment
+                    p = Process(target= train, args=(config, ))                                         # Using the Process() to wrap the experiment
+                    p.start()                                                                           # Start the experiment
 
 
 if __name__ == "__main__":
-    run()
+    run()                                                           # Main function
--- src/svm.py
+++ src/svm.py
@@ -3,66 +3,118 @@ import numpy as np
 import time
 import pickle
 import json
-import os
 from sklearn.svm import SVC
 from sklearn.metrics import classification_report
 from kernel import Histogram_Intersection_Kernel
-from utils import load_config, check_path
-from dataloader import DataLoader
+from utils import check_path
 
 
 class SVM(object):
+    """
+    Wrap of sklearn.svm.SVC, including some base operation like traning, evaluation, save_model. etc
+    """
     def __init__(self, config: Dict) -> None:
-        super().__init__()
+        """
+        Initialize
 
-        self.save_path = config['save_path']
-        check_path(self.save_path)
+        Args:
+            config (Dict): [the config dict evolving some setting]
+        """
+        super().__init__()                                                          # Super class initialize
 
-        self.kernel = self._judge_kernel(config['kernel'])
-        self.model = SVC(kernel=self.kernel, gamma='auto')
+        self.save_path = config['save_path']                                        # Read the save path setting
+        check_path(self.save_path)                                                  # Check whether this path exists, if not then create it 
+
+        self.kernel = self._judge_kernel(config['kernel'])                          # Using the _judge_kernel() to read the kernel setting
+        self.model = SVC(kernel=self.kernel, gamma='auto')                          # Instantiate the SVC model
 
     def _judge_kernel(self, kernel_name: str) -> Union[Callable, str]:
-        if kernel_name == "Histogram":
+        """
+        Judge which type of kernel will be used
+
+        Args:
+            kernel_name (str): [the kernel string in confit dict]
+
+        Raises:
+            ValueError: [if the kernel name not reasonable, then the error raises]
+
+        Returns:
+            Union[Callable, str]: [the kernel function (user defined) or the kernel name string (already exists)]
+        """
+        if kernel_name == "Histogram":                                              # If use the histogram kernel, then use the function defined in kernel.py
             kernel = Histogram_Intersection_Kernel
-        elif kernel_name in ['rbf', 'poly', 'linear', 'sigmoid']:
+        elif kernel_name in ['rbf', 'poly', 'linear', 'sigmoid']:                   # If use the four built-in kernel, then just return the kernel string
             kernel = kernel_name
-        else:
-            raise ValueError(f"No such kind kernel: {config['kernel']}")
-        return kernel
+        else:                                                                       # If the kernel name not reasonable, then raise error
+            raise ValueError(f"No such kind kernel: {config['kernel']}")            
+        return kernel                                                               # Return the kernel string or Callable function
 
     def train(self, data: np.array, label: np.array) -> None:
-        print("-------- Start training --------")
-        start = time.time()
-        self.model.fit(data, label)
-        print("--------- Training over --------")
-        print(f"Training takes time {time.time() - start}")
+        """
+        Train the SVC model using the train_set
+
+        Args:
+            data (np.array): [train_set images data with shape (sample_num, 28*28)]
+            label (np.array): [train_set labels data with shape (sample_num, )]
+        """
+        print("-------- Start training --------")                                   # Print log
+        start = time.time()                                                         # Count the start time
+        self.model.fit(data, label)                                                 # Fit the train data
+        print("--------- Training over --------")                                   # Print log
+        print(f"Training takes time {time.time() - start}")                         # Print log about train time
 
     def evaluate(self, data: np.array, label: np.array) -> None:
-        pred = self.model.predict(data)
-        print(f"Classification Report: \n{classification_report(label, pred)}")
+        """
+        Evaluate the model
+
+        Args:
+            data (np.array): [image data with shape (sample_num, 28*28)]
+            label (np.array): [label data with shape (sample_num, )]
+        """
+        pred = self.model.predict(data)                                             # Make the prediction using the trained model
+        print(f"Classification Report: \n{classification_report(label, pred)}")     # Print logger with the output report
 
     def evaluate_and_save(self, data: np.array, label: np.array) -> None:
-        pred = self.model.predict(data)
-        report = classification_report(label, pred, output_dict=True)
+        """
+        Evaluate the model and save the report with model
+
+        Args:
+            data (np.array): [image data with shape (sample_num, 28*28)]
+            label (np.array): [label data with shape (sample_num, )]
+        """
+        pred = self.model.predict(data)                                             # Make the prediction using the trained model
+        report = classification_report(label, pred, output_dict=True)               # Get the prediction report
         
-        self.save_model()
-        self.save_report(report)
+        self.save_model()                                                           # Save model file
+        self.save_report(report)                                                    # Save report file
 
     def save_report(self, report: Dict) -> None:
-        with open(self.save_path + 'result.json', 'w') as f:
-            json.dump(report, f)
-        print(f"----- Save result report to {self.save_path + 'result.json'} -----")
+        """
+        Save the prediction report which has some data analyze
+
+        Args:
+            report (Dict): [the report dictionary]
+        """
+        with open(self.save_path + 'result.json', 'w') as f:                            # Open the file 
+            json.dump(report, f)                                                        # Using json.dump() to transform the dict to json type
+        print(f"----- Save result report to {self.save_path + 'result.json'} -----")    # Print log
 
     def save_model(self) -> None:
-        model_path = self.save_path + f'model.pickle'
+        """
+        Save model as pickle file
+        """
+        model_path = self.save_path + f'model.pickle'                               # Get the exact save path 
 
-        with open(model_path, 'wb') as f: #以二进制写的方式打开文件
-            pickle.dump(self.model, f) 
-        print(f"------ Model saved to {model_path} -------") #打印日志
+        with open(model_path, 'wb') as f:                                           # Open the file with binary write mode
+            pickle.dump(self.model, f)                                              # Save the model file
+        print(f"------ Model saved to {model_path} -------")                        # Print log
 
     def load_model(self) -> None:
-        model_path = self.save_path + f'model.pickle'
+        """
+        Load model from pickle file
+        """
+        model_path = self.save_path + f'model.pickle'                               # Get the exact file path
         
-        with open(model_path, 'rb') as f: #以二进制读的方式打开文件
-            self.model = pickle.load(f)
-        print(f"------ Loaded model from {model_path} -------")
+        with open(model_path, 'rb') as f:                                           # Open the file with binary read mode
+            self.model = pickle.load(f)                                             # Load the model
+        print(f"------ Loaded model from {model_path} -------")                     # Print log
